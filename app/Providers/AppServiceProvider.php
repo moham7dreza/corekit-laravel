36c20c7134beca9491f27a1ee3453d92288bb413
Mohamadreza Rezaei <mohammadreza.rezaei@shab.ir>
2026-02-21 13:37:53 +0330

    :art: improve health checks

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,9 +7,22 @@ use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
+use Spatie\Health\Checks\Checks\BackupsCheck;
+use Spatie\Health\Checks\Checks\CacheCheck;
+use Spatie\Health\Checks\Checks\DatabaseCheck;
+use Spatie\Health\Checks\Checks\DatabaseConnectionCountCheck;
+use Spatie\Health\Checks\Checks\DatabaseSizeCheck;
+use Spatie\Health\Checks\Checks\DatabaseTableSizeCheck;
 use Spatie\Health\Checks\Checks\DebugModeCheck;
 use Spatie\Health\Checks\Checks\EnvironmentCheck;
+use Spatie\Health\Checks\Checks\HorizonCheck;
 use Spatie\Health\Checks\Checks\OptimizedAppCheck;
+use Spatie\Health\Checks\Checks\PingCheck;
+use Spatie\Health\Checks\Checks\QueueCheck;
+use Spatie\Health\Checks\Checks\RedisCheck;
+use Spatie\Health\Checks\Checks\RedisMemoryUsageCheck;
+use Spatie\Health\Checks\Checks\ScheduleCheck;
+use Spatie\Health\Checks\Checks\UsedDiskSpaceCheck;
 use Spatie\Health\Facades\Health;
 
 class AppServiceProvider extends ServiceProvider
@@ -28,7 +41,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->configureDefaults();
-        $this->registerHealthChecks();
+        $this->runHealthChecks();
     }
 
     /**
@@ -53,12 +66,58 @@ class AppServiceProvider extends ServiceProvider
         );
     }
 
-    private function registerHealthChecks(): void
+    private function runHealthChecks(): void
     {
+        if ($this->app->runningUnitTests()) {
+            return;
+        }
+
         Health::checks([
-            OptimizedAppCheck::new(),
+            DatabaseCheck::new(),
+
+            CacheCheck::new(),
+
+            QueueCheck::new(),
+
+            RedisCheck::new(),
+
+            BackupsCheck::new(),
+
+            EnvironmentCheck::new()
+                ->expectEnvironment(config()->string('app.env')),
+
+            DatabaseTableSizeCheck::new()
+                ->table('users', maxSizeInMb: 1_000),
+
+            DatabaseSizeCheck::new()
+                ->failWhenSizeAboveGb(errorThresholdGb: 0.1),
+
+            DatabaseConnectionCountCheck::new()
+                ->warnWhenMoreConnectionsThan(50)
+                ->failWhenMoreConnectionsThan(100),
+
             DebugModeCheck::new(),
-            EnvironmentCheck::new(),
+
+            OptimizedAppCheck::new(),
+
+            PingCheck::new()
+                ->url(config()->string('app.url'))
+                ->timeout(2)
+                ->retryTimes(3)
+                ->label('App'),
+
+            HorizonCheck::new(),
+
+            UsedDiskSpaceCheck::new()
+                ->warnWhenUsedSpaceIsAbovePercentage(90)
+                ->failWhenUsedSpaceIsAbovePercentage(95),
+
+            ScheduleCheck::new()
+                ->heartbeatMaxAgeInMinutes(2),
+
+            RedisMemoryUsageCheck::new()
+                ->warnWhenAboveMb(900)
+                ->failWhenAboveMb(1000),
         ]);
     }
 }
