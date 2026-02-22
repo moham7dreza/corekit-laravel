199d04bf742f7884f751c8fd3a301f4bc0f75684
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2026-02-22 23:10:35 +0330

    :package: add munafio/chatify, laravel/reverb

---
+++ app/Models/ChFavorite.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Chatify\Traits\UUID;
+use Illuminate\Database\Eloquent\Model;
+
+class ChFavorite extends Model
+{
+    use UUID;
+}
---
+++ app/Models/ChMessage.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Chatify\Traits\UUID;
+use Illuminate\Database\Eloquent\Model;
+
+class ChMessage extends Model
+{
+    use UUID;
+}
