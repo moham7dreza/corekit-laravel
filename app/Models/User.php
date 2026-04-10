47f03645528a7060ec8a6c3241cb6c9b86b2ba83
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2026-04-10 22:04:01 +0330

    :art: add mobile and verified at to users table

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,31 +24,24 @@ use Spatie\Permission\Traits\HasRoles;
 class User extends Authenticatable implements FilamentUser, MustVerifyEmail
 {
     use AuthenticationLoggable;
-
     use HasApiTokens;
 
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable, TwoFactorAuthenticatable;
+    use HasFactory;
+
     use HasRoles;
     use HasUiPreferences;
     use LogsActivity;
+    use Notifiable;
+    use TwoFactorAuthenticatable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
+        'mobile',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'two_factor_secret',
@@ -56,16 +49,12 @@ class User extends Authenticatable implements FilamentUser, MustVerifyEmail
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     #[Override]
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
+            'mobile_verified_at' => 'datetime',
             'password' => 'hashed',
             'two_factor_confirmed_at' => 'datetime',
             'ui_preferences' => 'array',
