9fa9004cd3f8f0100796b8955d6099cbac9dd0cb
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2026-04-13 13:28:59 +0330

    :sparkles: add new raw monitoring module

---
+++ modules/monitoring/src/Providers/MonitoringServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Modules\Monitoring\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class MonitoringServiceProvider extends ServiceProvider
+{
+    public function register(): void {}
+
+    public function boot(): void {}
+}
