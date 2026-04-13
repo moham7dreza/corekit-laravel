d442b7859a6204c66c40ae5da1792b6d959a497a
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2026-04-13 14:00:11 +0330

    :art: update providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,23 +18,6 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
 use Laravel\Pulse\Facades\Pulse;
 use Override;
-use Spatie\Health\Checks\Checks\BackupsCheck;
-use Spatie\Health\Checks\Checks\CacheCheck;
-use Spatie\Health\Checks\Checks\DatabaseCheck;
-use Spatie\Health\Checks\Checks\DatabaseConnectionCountCheck;
-use Spatie\Health\Checks\Checks\DatabaseSizeCheck;
-use Spatie\Health\Checks\Checks\DatabaseTableSizeCheck;
-use Spatie\Health\Checks\Checks\DebugModeCheck;
-use Spatie\Health\Checks\Checks\EnvironmentCheck;
-use Spatie\Health\Checks\Checks\HorizonCheck;
-use Spatie\Health\Checks\Checks\OptimizedAppCheck;
-use Spatie\Health\Checks\Checks\PingCheck;
-use Spatie\Health\Checks\Checks\QueueCheck;
-use Spatie\Health\Checks\Checks\RedisCheck;
-use Spatie\Health\Checks\Checks\RedisMemoryUsageCheck;
-use Spatie\Health\Checks\Checks\ScheduleCheck;
-use Spatie\Health\Checks\Checks\UsedDiskSpaceCheck;
-use Spatie\Health\Facades\Health;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -48,7 +31,6 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->configureGate();
         $this->configurePulse();
-        $this->configureHealthChecks();
         $this->configureCommands();
         $this->configureModel();
         $this->configureVite();
@@ -58,61 +40,6 @@ class AppServiceProvider extends ServiceProvider
         $this->configureQueue();
         $this->configureEloquentRelation();
         $this->configureDate();
-    }
-
-    private function configureHealthChecks(): void
-    {
-        if ($this->app->runningUnitTests()) {
-            return;
-        }
-
-        Health::checks([
-            DatabaseCheck::new(),
-
-            CacheCheck::new(),
-
-            QueueCheck::new(),
-
-            RedisCheck::new(),
-
-            BackupsCheck::new(),
-
-            EnvironmentCheck::new()
-                ->expectEnvironment(config()->string('app.env')),
-
-            DatabaseTableSizeCheck::new()
-                ->table('users', maxSizeInMb: 1_000),
-
-            DatabaseSizeCheck::new()
-                ->failWhenSizeAboveGb(errorThresholdGb: 0.1),
-
-            DatabaseConnectionCountCheck::new()
-                ->warnWhenMoreConnectionsThan(50)
-                ->failWhenMoreConnectionsThan(100),
-
-            DebugModeCheck::new(),
-
-            OptimizedAppCheck::new(),
-
-            PingCheck::new()
-                ->url(config()->string('app.url'))
-                ->timeout(2)
-                ->retryTimes(3)
-                ->label('App'),
-
-            HorizonCheck::new(),
-
-            UsedDiskSpaceCheck::new()
-                ->warnWhenUsedSpaceIsAbovePercentage(90)
-                ->failWhenUsedSpaceIsAbovePercentage(95),
-
-            ScheduleCheck::new()
-                ->heartbeatMaxAgeInMinutes(2),
-
-            RedisMemoryUsageCheck::new()
-                ->warnWhenAboveMb(900)
-                ->failWhenAboveMb(1000),
-        ]);
     }
 
     private function configureGate(): void
---
+++ config/performance-log.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'command_log_disabled' => false,
+    'command_log_sampling_rate' => 0.05,
+    'job_log_disabled' => false,
+    'job_log_sampling_rate' => 0.05,
+];
